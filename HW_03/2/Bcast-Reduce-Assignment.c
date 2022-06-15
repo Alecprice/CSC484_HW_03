b835d8794ffd4562fa76b18d19986892db3731d8
Alecprice <52548136+Alecprice@users.noreply.github.com>
2022-06-14 23:36:04 -0400

    Add files via upload

---
+++ HW_03/2/Bcast-Reduce-Assignment.c
@@ -0,0 +1,72 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <mpi.h>
+ 
+
+int main(void)
+ 
+{
+	int my_rank, comm_sz;
+	int i;
+    int Count = 4;
+	int Number[4];
+	int Result[4];
+	int PartialResult[4];
+
+    MPI_Init(NULL, NULL);
+
+    MPI_Comm_size(MPI_COMM_WORLD, &comm_sz);
+    MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
+
+
+    if(my_rank == 0)
+	{
+		printf("The input sequence is: \n");
+
+		for (i = 0; i < Count; i++)
+		{
+			Number[i] = i;
+			printf("%d ", Number[i]);
+		}
+
+		printf("\n");
+    }
+
+	// Process 0 sends data to all of the processes
+    MPI_Bcast(Number, Count, MPI_INT, 0, MPI_COMM_WORLD);
+
+	for (i = 0; i < Count; i++)
+   	 {
+      	 // Number[i] += my_rank;
+      	  PartialResult[i] = Number[i] + my_rank;
+
+	 // printf("My Rank: %d \n", my_rank);
+       	 
+      	 // printf("Partial result in forloop: %d \n", PartialResult);
+
+        }
+	MPI_Reduce(&PartialResult , &Result, 4, MPI_INT, MPI_PROD, 0, MPI_COMM_WORLD);
+
+	//printf(" result after reduce: %d \n", Result);
+
+	// Print out the result 
+	if (my_rank == 0)
+	{
+		printf("The output sequence is: \n");
+
+		for (i = 0; i < Count; i++)
+		{
+			
+			printf("%d ", Result[i]);
+		}
+
+	
+
+		printf("\n");
+	}
+
+	MPI_Finalize();
+
+	return 0;
+}
+
